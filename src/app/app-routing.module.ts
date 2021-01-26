f5b9107f548ff55769d4b85c47fdeedeccf7e9e6
Brooklyn Hairston <bhairston@my365.bellevue.edu>
2021-01-25 21:25:52 -0500

    updating

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [
     component: ContactComponent
   },
   {
-    path: '/composer-details/:composerId',
+    path: 'composer-details/:composerId',
     component: ComposerDetailsComponent
   }
 ];
